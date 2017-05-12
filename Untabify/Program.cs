bd0a9189b2d480270fd07581692f42a9461ed603
ShTair <a@vsh.jp>
2017-05-12 17:34:34 +0900

    完成

--- Untabify/Program.cs
+++ Untabify/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Untabify
 {
@@ -10,6 +10,40 @@ namespace Untabify
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0 || !File.Exists(args[0]))
+            {
+                var target = new Target
+                {
+                    Path = AppDomain.CurrentDomain.BaseDirectory,
+                    SpaceCount = 4,
+                    Exts = new List<string> { ".htm", ".html", ".css", ".js", ".json" },
+                };
+
+                var json = JsonConvert.SerializeObject(target, Formatting.Indented);
+                var name = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "test.json");
+                File.WriteAllText(name, json);
+
+                return;
+            }
+            else
+            {
+                var json = File.ReadAllText(args[0]);
+                var target = JsonConvert.DeserializeObject<Target>(json);
+
+                var spaces = string.Join("", Enumerable.Range(0, target.SpaceCount).Select(_ => " "));
+
+                foreach (var name in Directory.EnumerateFiles(target.Path, "*", SearchOption.AllDirectories))
+                {
+                    var ext = Path.GetExtension(name).ToLower();
+                    if (!target.Exts.Contains(ext)) continue;
+
+                    Console.WriteLine(name);
+
+                    var text = File.ReadAllText(name);
+                    text = text.Replace("\t", spaces);
+                    File.WriteAllText(name, text);
+                }
+            }
         }
     }
 }
---
+++ Untabify/Target.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Untabify
+{
+    class Target
+    {
+        public string Path { get; set; }
+
+        public int SpaceCount { get; set; }
+
+        public List<string> Exts { get; set; }
+    }
+}
